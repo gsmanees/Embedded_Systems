f0f6d6c0eb06122435a8bcb9339817053ecc5dbb
Anees Kokadan <gsmanees@gmail.com>
2025-08-05 07:38:06 +0530

    first c program added and committed

---
+++ My_First_C_program/MyFirstCprogram.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Anees Kokadan\n");
+    printf("Kokadans Kalathingal\n");
+    printf("Puliyacode PO\n");
+    printf("Kuzhimanna via\n");
+    printf("673641 pin\n");
+    printf("Malappuram Dist\n");
+    printf("Kerala\n");
+    printf("India\n");
+    printf("Mob: 9946339258\n");
+    printf("Email:mail@gsmanees.com\n");
+    printf("Website: www.gsmanees.com\n");
+    
+
+    return 0;
+}
