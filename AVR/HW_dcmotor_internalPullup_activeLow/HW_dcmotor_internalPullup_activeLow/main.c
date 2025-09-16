98bb72a13689b0b860852ecf91abe9a22fdf6449
Anees Kokadan <gsmanees@gmail.com>
2025-09-16 23:15:51 +0530

    AVR folder updated with dcmotor_direct, dcmotor_activeHigh and dcmotor_activeHigh concepts

---
+++ AVR/HW_dcmotor_internalPullup_activeLow/HW_dcmotor_internalPullup_activeLow/main.c
@@ -0,0 +1,55 @@
+/*
+ * HW_dcmotor_internalPullup_activeLow.c
+ *
+ * Created: 16-09-2025 7.25.02 AM
+ * Author : anees
+ */ 
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+void CW(){
+	PORTB|=(1<<PORTB5);
+	PORTB&=~(1<<PORTB4);
+	_delay_ms(3000);
+}
+
+void STP(){
+	PORTB&=~(1<<PORTB5);
+	PORTB&=~(1<<PORTB4);
+	_delay_ms(1000);
+}
+
+void CCW(){
+	PORTB&=~(1<<PORTB5);
+	PORTB|=(1<<PORTB4);
+	_delay_ms(3000);
+}
+
+int main(void)
+{
+	DDRD&=~(1<<DDD2);
+	PORTD|=(1<<PORTD2);
+	DDRB|=(1<<5);
+	DDRB|=(1<<4);
+	while (1)
+	{
+		if (!(PIND&(1<<PIND2)))
+		{
+			CW(); // Clock wise
+			STP(); // Stop
+			CCW(); // Counter clock wise
+			STP(); // Stop
+		}
+		else
+		{
+			STP(); // Stop
+			
+		}
+		
+		
+		
+	}
+}
+
---
+++ AVR/dcmotor_direct/dcmotor_direct/main.c
@@ -0,0 +1,43 @@
+/*
+ * dcmotor_direct.c
+ *
+ * Created: 16-09-2025 5.57.54 AM
+ * Author : anees
+ */ 
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+void CW(){
+	PORTB|=(1<<PORTB5);
+	PORTB&=~(1<<PORTB4);
+	_delay_ms(3000);
+}
+
+void STP(){
+	PORTB&=~(1<<PORTB5);
+	PORTB&=~(1<<PORTB4);
+	_delay_ms(1000);
+}
+
+void CCW(){
+	PORTB&=~(1<<PORTB5);
+	PORTB|=(1<<PORTB4);
+	_delay_ms(3000);
+}
+
+int main(void)
+{
+	
+	DDRB|=(1<<5);
+	DDRB|=(1<<4);
+	while (1)
+	{
+		
+			CW(); // Clock wise
+			STP(); // Stop
+			CCW(); // Counter clock wise
+			STP(); // Stop
+	}
+}
