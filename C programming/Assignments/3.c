80fe2a1ca0468f07bb76e4e48155732c865fb14d
Anees Kokadan <gsmanees@gmail.com>
2026-05-02 16:28:04 +0530

    C assignment qnswer 3 added

---
+++ C programming/Assignments/3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i, f=1;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+     if (n==0)
+     {
+        f=1;
+     }
+
+     else
+     {
+        for (i=1; i<=n;i++)
+        {
+            f*=i;
+            // 5 x 4 x 3 x 2 x 1
+        }
+     }
+
+     printf("Factorial of the number %d is : %d", n, f);
+}
